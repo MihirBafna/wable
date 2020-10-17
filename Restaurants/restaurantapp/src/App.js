a0a7a5191e6b7ac9ee9a26ba55696fd59c39bded
Apuroop Mutyala <apumutyala@gmail.com>
2020-10-17 04:29:24 -0400

    added merge things

--- Restaurants/restaurantapp/src/App.js
+++ Restaurants/restaurantapp/src/App.js
@@ -14,11 +14,6 @@ class App extends React.Component {
             href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
             integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
             crossorigin="anonymous" />
-<<<<<<< HEAD
-            <Table />
-          {/* <img src={logo} className="App-logo" alt="logo" /> */}
-=======
->>>>>>> d3c120156fb303fa85cec966ae555e9f7d2b87c2
         </header>
         <NavBar />
         
