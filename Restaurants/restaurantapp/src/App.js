8662e39c1c218a35e4186c3eeaaee3d354cafd39
Apuroop Mutyala <apumutyala@gmail.com>
2020-10-17 04:27:43 -0400

    added table.js

--- Restaurants/restaurantapp/src/App.js
+++ Restaurants/restaurantapp/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 // import './App.css';
-
+import Table from './components/Table'
 
 class App extends React.Component {
   render() {
@@ -13,6 +13,7 @@ class App extends React.Component {
             href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
             integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
             crossorigin="anonymous" />
+            <Table />
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
         </header>
         <NavBar />
---
+++ Restaurants/restaurantapp/src/components/Table.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Table extends React.Component {
+    render() {
+        rect = fillRect(10, 10, 200, 200)
+        return(
+            rect
+        );
+    }
+}
+
+export default Table;
+
+
+
+
