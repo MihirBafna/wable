f3f47c5b1882591062377ada71652f5f7a4de6f4
vikranthkeerthipati <vkeerthipati@gatech.edu>
2020-10-17 21:28:49 -0400

    updated authentication to HMAC

--- Customer/customerapp/src/backend/HMACAuth.js
+++ Customer/customerapp/src/backend/HMACAuth.js
@@ -1,43 +1,68 @@
-const convertVariables = function(templateContent) {
-    const regexPattern = /({{(.*?)}})/g;
-    let convertedContent = templateContent;
-    let matchedVar = new RegExp(regexPattern).exec(convertedContent);
-    while (matchedVar !== null) {
-        const variableReplacement = matchedVar[1];
-        const variableName = matchedVar[2];
-        const variableValue = postman.getEnvironmentVariable(variableName) || postman.getGlobalVariable(variableName);
-        convertedContent = convertedContent.replace(variableReplacement, variableValue);
-        matchedVar = new RegExp(regexPattern).exec(convertedContent);
+global.XMLHttpRequest = require('xhr2')
+var sha512 = require('crypto-js/sha512');
+var Base64 = require('crypto-js/enc-base64');
+
+// Calculate the access key credentials for a HTTP request
+let sharedKey = "c8abc04534714eb4ab2d884bf1b9f388"
+let secretKey = "923c9543fb2e47538a0ef6faae7c2e75"
+const CalculateAccessKeyCredentials = (request) => {
+    // console.log(request._headers["Date"])
+    // console.log(request.getResponseHeader("nep-organization"))
+    // let requestDateString = request.getResponseHeader("Date")
+    console.log(request)
+    // Parse date string from HTTP Date header to a native representation
+    let requestDate = request._headers.Date;
+    // Generate one-time key to use for HMAC generation
+    let key = uniqueKey(requestDate)
+
+    // Get content to sign in the HMAC
+    let contentToSign = signableContent(request)
+
+    // Calculate the HMAC
+    let hmac = sha512(contentToSign, key);
+
+    // Encode the HMAC in Base64 encoding (RFC 4648)
+    let encodedSignature = Base64.stringify(hmac);
+
+    // Return the full credentials string for the HTTP Authorization header
+    console.log(sharedKey + ":" + encodedSignature)
+    return sharedKey + ":" + encodedSignature
+}
+
+function uniqueKey(requestDate) {
+    // Convert the native date value into a ISO-8601 string.  It should have
+    // 3 digits of precision in the sub-seconds field, and those values will
+    // be `0`s (for instance, "2018-10-10T00:00:00.000Z")
+
+    let iso8601String = requestDate.toISOString();
+
+    // Concat secret key with date string to generate one-time key for signing
+    return secretKey + iso8601String
+}
+
+function signableContent(request) {
+    // HTTP method and path/query string are required parameters
+    let params = [request.Method, request.EscapedPathAndQueryString]
+
+    // All headers used in HMAC calculation
+    let signableHeaders = [
+        "Content-Type",
+        "Content-MD5",
+        "nep-application-key",
+        "nep-correlation-id",
+        "nep-organization",
+        "nep-service-version"
+    ]
+
+    // Only include header values from the request in the signable content that are
+    // sent in the HTTP request.
+    for (signableHeader in signableHeaders) {
+        if (request._headers[signableHeader] != undefined) {
+            params.push(request.Headers.GetValue(signableHeader))
+        }
     }
-    return convertedContent;
+
+    return params.join("\n")
 }
-// Extracts the signable content from the request
-const signableContent = function() {
-    const requestPath = convertVariables(request.url.trim()).replace(/^https?:\/\/[^\/]+\//, '/');
-    const params = [
-        request.method,
-        requestPath,
-        request.headers['content-type'],
-        request.headers['content-md5'],
-        convertVariables(request.headers['nep-organization'])
-    ];
-    return params.filter(p => p && p.length > 0).join('\n');
-}
-// Generates a unique date-based signing key
-const uniqueKey = function(date) {
-    const nonce = date.toISOString().slice(0, 19) + '.000Z';
-    return postman.getEnvironmentVariable('bsp-secret-key') + nonce;
-}
-// Calculates the HMAC signature
-const calculateSignature = function() {
-    const date = new Date();
-    date = date.toGMTString();
-    const key = uniqueKey(date);
-    const sc = signableContent();
-    const hmac = CryptoJS.HmacSHA512(sc, key);
-    return CryptoJS.enc.Base64.stringify(hmac);
-}
-// Stores the generated HMAC signature under the access key
-const signature = calculateSignature();
-const sharedKey = postman.getEnvironmentVariable('bsp-shared-key');
-postman.setEnvironmentVariable('bsp-access-key', `AccessKey ${sharedKey}:${signature}`);
+
+module.exports.CalculateAccessKeyCredentials = CalculateAccessKeyCredentials
--- Customer/customerapp/src/backend/shoppingCart.js
+++ Customer/customerapp/src/backend/shoppingCart.js
@@ -1,29 +1,31 @@
 var request = require('request');
 var username ='dea06c54-d713-404a-988b-54c204407219';
-var password = 'V39v39123!'
-var auth = 'Basic ' + Buffer.from(username + ':' + password).toString('base64');
-headers = {
-    'Accept':'application/json',
-    'content-type': 'application/json',  
-    'Authorization': auth,  
-    'nep-organization': 'ur-hack',   
-    'nep-service-version': '2:1' 
-}
+var HMACAuth = require('./HMACAuth')
+global.XMLHttpRequest = require('xhr2')
+var xhr = new XMLHttpRequest();
+
 id = '66979a6495414081ae5930389b1c60ff'
 // urlWanted = 'https://gateway-staging.ncrcloud.com/site/sites/'+id;
-urlWanted = 'https://gateway-staging.ncrcloud.com/order/3/orders/1'
-const options = {
-    url : urlWanted,
-    headers: headers,
+let urlWanted = 'https://gateway-staging.ncrcloud.com/order/3/orders/1'
 
-}
-function callback(error, response, body) {
-    if (!error && response.statusCode == 200) {
-      const info = JSON.parse(body);
-      console.log(info.stargazers_count + " Stars");
-      console.log(info.forks_count + " Forks");
+xhr.open('POST', urlWanted, true);
+xhr.setRequestHeader('Access-Control-Allow-Origin', '*')
+xhr.setRequestHeader('Access-Control-Expose-Headers', 'Date')
+xhr.setRequestHeader('Accept','application/json')
+xhr.setRequestHeader('content-type','application/json',)
+xhr.setRequestHeader('nep-organization', 'ur-hack')
+xhr.setRequestHeader('nep-service-version', '2:1')
+xhr.setRequestHeader('Authorization','AccessKey ' + HMACAuth.CalculateAccessKeyCredentials(xhr))
+
+  xhr.onload = function () {
+    console.log('DONE: ', xhr.responseText);
+  };
+console.log(xhr.status)
+
+xhr.onreadystatechange = function () {
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        console.log("Success")
     }
-    console.log(response.body)
-  }
+}
 
-  let response = request.post(options, callback)
+
