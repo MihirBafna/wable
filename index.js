af97cd216dc22abce244154e330468818801b967
vikranthkeerthipati <vkeerthipati@gatech.edu>
2020-10-17 23:07:39 -0400

    debugging server

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req,res) => {
+    console.log("reacher");
     res.sendFile("./Wable-Server/wable-server-app/public/index.html")
 });
 
