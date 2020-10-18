652c3b06fa92b9ad37d2fb1c9e9874141179f9e3
vikranthkeerthipati <vkeerthipati@gatech.edu>
2020-10-18 01:08:27 -0400

    web server updates

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'Wable-Server')));
+
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'Wable-Server', '/wable-server-app/public/index.html'));
+});
+
+app.listen(process.env.PORT || 8080);
