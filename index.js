de34cc4382c9378719b2d38f0782f07693933779
vikranthkeerthipati <vkeerthipati@gatech.edu>
2020-10-17 23:10:37 -0400

    debugging

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const port = 80;
 
 app.get("/", (req,res) => {
     console.log("reacher");
     res.sendFile("./Wable-Server/wable-server-app/public/index.html")
 });
-
-app.listen(80);
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
