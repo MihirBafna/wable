94a9a4dc96f4a485abb10fb8d1414a562c1a1c4f
vikranthkeerthipati <vkeerthipati@gatech.edu>
2020-10-18 08:01:58 -0400

    managed QR codes

---
+++ Wable-Server/wable-server-app/src/components/GetStarted.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class GetStarted extends React.Component{
+    render(){
+        return(
+            <h1>Working</h1>
+        );
+    }
+}
+
+export default GetStarted;
