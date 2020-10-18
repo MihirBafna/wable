ff477dbdac6c9c02c470f6a382c0c03449d1a4da
Apuroop Mutyala <apumutyala@gmail.com>
2020-10-18 05:07:52 -0400

    committ

--- Customer/customerapp/src/components/MenuItem.js
+++ Customer/customerapp/src/components/MenuItem.js
@@ -1,15 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MenuDropdown from './MenuDropdown'
-import { Container, Row, Col } from 'reactstrap';
+import { Container, Row, Col, Button, Modal, ModalHeader, ModalBody, ModalFooter,} from 'reactstrap';
 import '../css/MenuItem.css'
 import Quantity from './Quantity'
+import ModalExample from './Modal'
+import { toggle, buttonLabel, modal, className} from 'react'
+
 
 class MenuItem extends React.Component{
     constructor(props){
         super(props);
 
     }
+    
     render(){
+
         return(
             <Container fluid>
 
@@ -22,14 +27,29 @@ class MenuItem extends React.Component{
                 </MenuDropdown>
                 
                 </Col>
-                <Col> <p>{this.props.desc}</p></Col>
+                <Col> <p>{this.props.desc}
+                <div>
+                    <Button color="danger" onClick={toggle}>{"Description"}</Button>
+                    <Modal isOpen={modal} toggle={toggle} className={className}>
+                    <ModalHeader toggle={toggle}>Modal title</ModalHeader>
+                    <ModalBody>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </ModalBody>
+                    <ModalFooter>
+                    <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
+                    <Button color="secondary" onClick={toggle}>Cancel</Button>
+                    </ModalFooter>
+                    </Modal>
+                </div>
+                
+                
+                </p></Col>
                 <Col id="price"><h5>{this.props.price}</h5></Col>
                 <Quantity />
             </Row> 
             </Container>
-        )
-
-    }
+        );
+        }
 }
 
 export default MenuItem;
